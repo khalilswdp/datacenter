b1303d49ca68e7797d4fc88a16eed354f34596eb
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-07-13 10:10:54 +0100

    initial commit: Navbar initialized

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild, ComponentFactoryResolver, ComponentRef, ViewContainerRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private ComponentFactoryResolver: ComponentFactoryResolver) {}
+
+}
