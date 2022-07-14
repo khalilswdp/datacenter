b2e9be0af04d7df0f1c5b91c8279aa1f90aaa5e0
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-07-14 16:39:23 +0100

    Add necessary interfaces + Generate random datacenter data and metrics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -18,5 +20,13 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  // 4.2.1 Component lifecycle
+  // Table 4.1 on lifecycle hooks
+  // https://angular.io/guide/lifecycle-hooks
+  // Types of components and their characteristics 4.3
+
+  // I like this:
+  // angular projects and demos to actually learn from
+  // https://angular.io/guide/lifecycle-hooks#oninit
+  // 4.4 Nesting components
+  // I need to understand the difference between content child and view child
 }
