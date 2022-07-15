151b5ff1df2ef7c1e25f64663cae2ced71f6a634
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-07-15 11:16:59 +0100

    Make into attribute selector and add comments for future explanations

--- src/app/nodes/nodes.component.ts
+++ src/app/nodes/nodes.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nodes',
+  // This is a css selector for the component
+  // It can be an element, an attribute or a class
+  // https://blog.knoldus.com/different-types-of-component-selectors-in-angular/
+  // 'selector' or '[selector]' or '.selector'
+  // <selector ***> or <*** selector> or <**** **** class="selector">
+  selector: '[app-nodes]',
   templateUrl: './nodes.component.html',
   styleUrls: ['./nodes.component.css']
 })
